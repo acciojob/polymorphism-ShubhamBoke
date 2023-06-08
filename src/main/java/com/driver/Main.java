002a0d6b40af4ae2bba59dbbf3ed139b21b4357f
Shubham Boke <shubhamboke.99@gmail.com>
2023-06-08 11:23:40 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
+import java.util.*;
+import org.w3c.dom.ls.LSOutput;
 
 public class Main {
+    static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
 
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        System.out.println(p.product(2, 5));
+        System.out.println(p.product(2, 5, 6));
+        System.out.println(p.product(2.5, 5.7));
+    }
 }
